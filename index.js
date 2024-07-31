5b0dd7b46a5477d08e6930b586ae27d36390abf3
Nick Christensen <nickachristensen@gmail.com>
2024-07-31 09:23:48 -0400

    Remove solution

--- index.js
+++ index.js
@@ -1,29 +1,4 @@
-// Function 1: calculateTax
-function calculateTax(amount) {
-  return amount * 0.10;
-}
 
-// Function 2: convertToUpperCase
-function convertToUpperCase(text) {
-  return text.toUpperCase();
-}
-
-// Function 3: findMaximum
-function findMaximum(num1, num2) {
-  return num1 > num2 ? num1 : num2;
-}
-
-// Function 4: isPalindrome
-function isPalindrome(word) {
-  const reversedWord = word.split('').reverse().join('');
-  return word === reversedWord;
-}
-
-// Function 5: calculateDiscountedPrice
-function calculateDiscountedPrice(originalPrice, discountPercentage) {
-  const discountAmount = (originalPrice * discountPercentage) / 100;
-  return originalPrice - discountAmount;
-}
 
 
 
